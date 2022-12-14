88eabdc88352823d8cef2243027ee688a897e8d8
zolarastaw <ashumarks@gmmail.com>
2022-12-14 22:35:54 +0100

    Negative and Posetive numbers

--- source/matrixindex.cpp
+++ source/matrixindex.cpp
@@ -1,37 +1,49 @@
-﻿// indices.cpp : Defines the entry point for the application.
+﻿// matrixindex.cpp : Defines the entry point for the application.
 //
 
 #include "matrixindex.h"
 #include<iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <random>
 
-using namespace std;
-#define row 10
-#define column 10
+// C++ program for generating a random number in a given range.
 
+
+#define rows 15
+#define cols 15
+int a[rows][cols], i, j;
 int main()
 {
+    
 
-
-	srand(time(NULL));
-
-	int a[row][column], i, j;
-
-	for (i = 0; i < row; ++i)
-	{
-		for (j = 0; j < column; ++j)
-		{
-			a[i][j] = rand() % 10;
-		}
-	}
-	for (i = 0; i < row; ++i)
-	{
-		for (j = 0; j < column; ++j)
-			std::cout << a[i][j] << '\t';
-		std::cout << '\n';
-	}
-
-	cout << "Hello CMake." << endl;
-	return 0;
+        int min = -100;
+        int max = 100;
+        
+        // seed for random generator
+        srand(time(NULL));
+            for (i = 0; i < rows; i++)
+             {
+                for (j = 0; j < cols; j++)
+                {           
+                    //a[i][j] = rand(10)- 5; // lie b/w 1 to 500;
+                   a[i][j] = min + rand() % ( max - min + 1 );
+                    
+                }
+            
+            }
+          
+                for (i = 0; i < rows; i++)
+                {
+                    for (j = 0; j < cols; j++)
+                        std::cout << a[i][j] << '\t';
+                    std::cout << '\n';
+                }
+        
+    
+             
+  
+    return 0;
 }
